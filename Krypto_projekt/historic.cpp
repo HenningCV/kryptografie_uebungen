59870d47108072e03bcdc4c222f57ca49602f929
HenningCV <henningvenhorst@gmail.com>
2023-04-12 10:50:35 +0200

    vignenere added

--- Krypto_projekt/historic.cpp
+++ Krypto_projekt/historic.cpp
@@ -2,8 +2,33 @@
 #include <string>
 
 std::string vigenere(std::string word, std::string key)
-{
+{	
 	std::string str = word;
+	
+	std::string keystr = "";
+
+	int strCharNumber = 0;
+	int keyCharNumber = 0;
+	int newCharNumber = 0;
+
+	while (keystr.length() < str.length())
+	{
+		keystr += key;
+	}
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		strCharNumber = str[i] - 65;
+		keyCharNumber = keystr[i] - 65;
+		newCharNumber = strCharNumber + keyCharNumber + 65;
+		if (newCharNumber > 90)
+		{
+			newCharNumber -= 26;
+		}
+		str[i] = newCharNumber;
+
+	}
+
 	
 
 	return str;
